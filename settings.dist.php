9873e0b308503fd23c4dd3527ae593e43fd8cd33
Gladhon <stony@onlain.ch>
2020-10-22 17:45:39 +0200

    update dist

--- settings.dist.php
+++ settings.dist.php
@@ -1,12 +1,11 @@
 <?php
 
-$config = [
-    'TOGGL_API_TOKEN'       => '',      // your toggl API Token
+return [
+    'TOGGL_API_TOKEN'       => '',      // your toggl API Token ( OR AS ENVIROMENT VARIABLE: TOGGL_TOKEN)
     'TOGGL_USER_AGENT'      => '',      // your toggl username (GoTom-email)
     'TOGGL_USER_IDS'        => '',      // your toggl id from profile.json
-    'DISPLAY_DATE_FORMAT'   => '',      // optional, a format to display the dates on readout e.g. 'Y-m-d' or 'd.m.Y', etc.
-    'START_YEAR'            => 2020,    // optional, 2017 by default, the year you started
-    'START_MONTH'           => 1,       // optional, 1 by default, the month you started
+    'DISPLAY_DATE_FORMAT'   => 'd.m.Y',      // optional, a format to display the dates on readout e.g. 'Y-m-d' or 'd.m.Y', etc.
+    'START_DATE'            => '',    // optional, 2017 by default, the year you started
     'DAYS_OFF'              => [],       // to configure part time work, weekly  days off, enter the day number for each day as array entry (1 = monday, 2 = tuesday, ...)
     'HALF_DAYS_OFF'         => [],       // to configure part time work, weekly half days off, enter the day number for each day as array entry (1 = monday, 2 = tuesday, ...)
     'VACATION_DAYS_AMOUNT'  => 5*5,     // 5 weeks à 5 days
@@ -19,3 +18,5 @@ $config = [
                                     ],
                                 ],
 ];
+
+
